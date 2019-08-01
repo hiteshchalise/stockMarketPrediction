744a28e433d2db74b812b664f24cbd0ddf437d58
Hitesh Chalise <hiteshchalise10@gmail.com>
2019-08-01 18:02:57 +0545

    added new input parameters

--- src/__init__.py
+++ src/__init__.py
@@ -10,10 +10,12 @@ def header(msg):
 
 
 class NeuralNetwork:
+    accuracy = 0
+
     def __init__(self, x, y):
         self.input = x
-        self.weights_1 = np.random.rand(self.input.shape[1], 6)
-        self.weights_2 = np.random.rand(6, 1)
+        self.weights_1 = np.random.randn(self.input.shape[1], 5)
+        self.weights_2 = np.random.randn(5, 1)
         self.y = y
         self.output = np.zeros(self.y.shape)
 
@@ -24,36 +26,22 @@ class NeuralNetwork:
 
     def backprop(self):
         # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
-        d_weights_2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
+        d_weights_2 = np.dot(self.layer1.T, (2 * (self.y - self.output) * sigmoid_derivative(self.output)))
         d_weights_1 = np.dot(self.input.T,
                              (np.dot(2 * (self.y - self.output) * sigmoid_derivative(self.output),
                                      self.weights_2.T) * sigmoid_derivative(self.layer1)))
 
-        # update the weights with the 153553derivative (slope) of the loss function
+        # update the weights with the derivative (slope) of the loss function
         self.weights_1 += d_weights_1
         self.weights_2 += d_weights_2
 
-    def SGD(self, training_data, epochs, mini_batch_size, eta):
-        n = len(training_data)
-        for j in range(epochs):
-            random.shuffle(training_data)
-            mini_batches = [
-                training_data[k:k + mini_batch_size] for k in range(0, n, mini_batch_size)]
-            for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batch, eta)
-            print("Epoch {0} complete".format(j))
-
-    def update_mini_batch(self, mini_batch, eta):
-        nabla_b = [np.zeros(b.shape) for b in self.biases]
-        nabla_w = [np.zeros(w.shape) for w in self.weights]
-        for x, y in mini_batch:
-            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
-            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
-            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-        self.weights = [w-(eta/len(mini_batch))*nw
-                        for w, nw in zip(self.weights, nabla_w)]
-        self.biases = [b-(eta/len(mini_batch))*nb
-                       for b, nb in zip(self.biases, nabla_b)]
+    def evaluate(self, testing_x, testing_y):
+        self.input = testing_x
+        self.y = testing_y
+        self.feedforward()
+        for index in range(self.y.size):
+            if int(self.output[index]) == self.y[index]:
+                NeuralNetwork.accuracy += 1
 
 
 def sigmoid(x):
@@ -61,7 +49,7 @@ def sigmoid(x):
 
 
 def sigmoid_derivative(sigmoid_output):
-    return sigmoid_output*(1-sigmoid_output)
+    return sigmoid_output * (1 - sigmoid_output)
 
 
 def normalize(data_col):
@@ -77,40 +65,50 @@ def normalize(data_col):
             min_of_row = row[0]
 
     for index, data in enumerate(data_col):
-        output[index] = (data[0] - min_of_row)/(max_of_row - min_of_row)
+        output[index] = (data[0] - min_of_row) / (max_of_row - min_of_row)
 
     return output
 
 
-use_cols = ['stock_symbol', 'Opening_Price', 'Max_Price', 'Min_Price', 'Closing_Price', 'Next_Day_Closing_Price']
+use_cols = ['stock_symbol', 'Total_Transactions', 'Total_Traded_Shares', 'Total_Traded_Amount',
+            'Opening_Price', 'Max_Price', 'Min_Price', 'Closing_Price', 'Next_Day_Closing_Price']
 
 df = pd.read_csv("stocks_data.csv", usecols=use_cols)
 data_np = df.to_numpy()
 
-input_x = data_np[:, [1, 2, 3, 4]]
-input_x_op = data_np[:, [1]]
-input_x_max_p = data_np[:, [2]]
-input_x_min_p = data_np[:, [3]]
-input_x_cp = data_np[:, [4]]
-y = data_np[:, [5]]
+input_x = data_np[:, [1, 2, 3, 4, 5, 6, 7]]
+input_x_tt = data_np[:, [1]]
+input_x_tts = data_np[:, [2]]
+input_x_tta = data_np[:, [3]]
+input_x_op = data_np[:, [4]]
+input_x_max_p = data_np[:, [5]]
+input_x_min_p = data_np[:, [6]]
+input_x_cp = data_np[:, [7]]
 
+closing_price = data_np[:, [7]]
+next_day_closing_price = data_np[:, [8]]
+diff = closing_price - next_day_closing_price
+y = (diff < 0).astype(int)
+
+normalized_x_tt = normalize(input_x_tt)
+normalized_x_tts = normalize(input_x_tts)
+normalized_x_tta = normalize(input_x_tta)
 normalized_x_op = normalize(input_x_op)
 normalized_x_max_p = normalize(input_x_max_p)
 normalized_x_min_p = normalize(input_x_min_p)
 normalized_x_cp = normalize(input_x_cp)
 
-normalized_x = np.concatenate((normalized_x_op, normalized_x_max_p, normalized_x_min_p, normalized_x_cp), axis=1)
-normalized_y = normalize(y)
+normalized_x = np.concatenate((normalized_x_tt, normalized_x_tts, normalized_x_tta,
+                               normalized_x_op, normalized_x_max_p, normalized_x_min_p, normalized_x_cp), axis=1)
 
-print(normalized_x.shape)
-print(normalized_y)
+split = 115164
+train_x, train_y = normalized_x[:split, :], y[:split, :]
+test_x, test_y = normalized_x[split:, :], y[split:, :]
 
-count = 0
-neural_net = NeuralNetwork(normalized_x, normalized_y)
-while count < 153553:
+neural_net = NeuralNetwork(train_x, train_y)
+for _ in range(1500):
     neural_net.feedforward()
     neural_net.backprop()
 
-    if count % 100 == 0:
-        print(count)
-    count += 1
+neural_net.evaluate(test_x, test_y)
+print(NeuralNetwork.accuracy/test_y.size)
